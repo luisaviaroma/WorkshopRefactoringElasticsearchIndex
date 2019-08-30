2eeac02afbb51e4aa0780e9005458629f4aeecb0
Leonardo Dimitri Bruni <l.bruni@luisaviaroma.com>
2019-08-30 14:47:07 +0200

    Modifica hash mapping webapp

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import StartTest from './App.test';
 import './index.css';
 
 var istest = window.location.hash === '#test';
-var isbook = window.location.hash === '#book';
+var isdemo = window.location.hash === '#demo';
 var isfifa = window.location.hash === '#fifa';
-var isdemo = !istest && !isbook && !isfifa;
+var isbook = !istest && !isdemo && !isfifa;
 
 if (isdemo || isbook || isfifa) {
   var root = document.getElementById('root');
